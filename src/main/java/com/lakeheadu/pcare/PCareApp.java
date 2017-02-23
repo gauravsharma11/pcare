d9b6afd6413cd0b187af00b8ac1a05fd45644a08
sarwagyakh <sarwagyakhosla@10.100.24.51>
2017-02-23 17:20:14 -0500

    1.Home page designed 2.partially login page designed by sarwagya khosla on 23th feb,2017

--- src/main/java/com/lakeheadu/pcare/PCareApp.java
+++ src/main/java/com/lakeheadu/pcare/PCareApp.java
@@ -2,7 +2,7 @@ package com.lakeheadu.pcare;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//lsof -w -n -i tcp:8080 kill -9 <process id>
 @SpringBootApplication(scanBasePackages={"com.lakeheadu.pcare.dao","com.lakeheadu.pcare.models", "com.lakeheadu.pcare.services" , "com.lakeheadu.pcare.controllers"})
 public class PCareApp {
 	
--- src/main/java/com/lakeheadu/pcare/controllers/MainController.java
+++ src/main/java/com/lakeheadu/pcare/controllers/MainController.java
@@ -1,14 +1,37 @@
 package com.lakeheadu.pcare.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.lakeheadu.pcare.models.Patient;
 
 @Controller
 public class MainController {
 	
-	@RequestMapping("/home")
+	@RequestMapping("/")
 	public String showHomePage(){
 		return "index";
 	}
+	
+	@RequestMapping("/login")
+	public String loginPage(){
+		return "login";
+	}
+
+	 @RequestMapping(value="/Adduser",method=RequestMethod.POST)
+	    public @ResponseBody String addUser(@RequestBody Patient patient, BindingResult result ){
+	        String returnText;
+	        if(!result.hasErrors()){
+	        //  userList.add(user);
+	            returnText = "User has been added to the list. Total number of users are "+patient.getName();
+	        }else{
+	            returnText = "Sorry, an error has occur. User has not been added to list.";
+	        }
+	        return returnText;
+	    }
 
 }
